e4a7a98943b4199e64ad991586f9226e531ff846
Bernath Kylian <bernath.kylian.pro@gmail.com>
2018-12-06 00:21:52 +0100

    Modification logo mobile
    Modification logo mobile

--- build/header.php
+++ build/header.php
@@ -4,7 +4,7 @@
 		<!-- Header Mobile -->
 		<div class="header-mobile clearfix" id="header-mobile">
 			<div class="header-mobile__logo">
-				<a href="_esports_index.html"><img src="assets/images/esports/logo.png" srcset="assets/images/esports/logo@2x.png 2x" alt="Alchemists" class="header-mobile__logo-img"></a>
+				<a href="_esports_index.html"><img src="#" srcset="#" alt="Alchemists" class="header-mobile__logo-img"></a>
 			</div>
 			<div class="header-mobile__inner">
 				<a id="header-mobile__toggle" class="burger-menu-icon"><span class="burger-menu-icon__line"></span></a>
